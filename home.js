828e366356a3ce3ec6e668777517b44447537f27
TekTaxi <tavienpollard@gmail.com>
2020-12-29 22:28:30 -0500

    fixed issue where multiple tabs showed at once

--- home.js
+++ home.js
@@ -1,11 +1,22 @@
-let pages = ["home", "about", "performances", "merch"];
-let current = -1;
+// this automatically sets the site to the homepage on load
+window.addEventListener("load", () => {
+    document.getElementById('home-btn').click();
+})
+// eventually we should manipulate the url so if you have a certain page selected it shows in the url
+// so if you reload it goes back to that tab
 
-function load(id) {
-    if ("content" in document.createElement("template") && id != current) {
-        const page = document.getElementById(pages[Number(id)]);
-        const node = document.importNode(page.content, true);
-        document.body.appendChild(node);
-        current = Number(id);
+function loadTab(id) {
+    // hides all the tabs
+    const openTabsNode = document.querySelectorAll('.current');
+    const openTabs = Array.from(openTabsNode);
+    if (openTabs !== undefined) {
+        for (const item in openTabs) {
+            openTabs[item].classList.remove('current');
+            openTabs[item].classList.add('hide');
+        }
     }
+    // shows the correct tab
+    const currentTab = document.getElementById(id);
+    currentTab.classList.add('current');
+    currentTab.classList.remove('hide');
 }
